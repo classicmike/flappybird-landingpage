8abf6e6f2ca86f2a76820984e598e5ad3babe324
Michael Tran <koramaiku@Michaels-iMac.local>
2015-08-03 23:14:38 +1000

    Progress

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('sass', function(){
 
 // Watch task
 gulp.task('watch', function(){
-	gulp.watch('site/js/*/js', [jshint]);
+	gulp.watch('site/js/*/js', ['jshint']);
 	gulp.watch('site/scss/*.scss', ['sass']);
 });
 
