038d74b82a959a1f0e12abf8df499a2fffb9a5af
Michael Tran <koramaiku@Michaels-iMac.local>
2015-08-06 22:01:11 +1000

    Finished

---
+++ site/js/app.js
@@ -0,0 +1,47 @@
+(function(){
+    var app = {};
+
+    /***--------- SCROLL TO Helper ------------ ***/
+    app.ScrollToHelper = {};
+
+    app.ScrollToHelper.scrollTo = function(id){
+        //scroll to
+        if(!id){
+            return;
+        }
+
+        var position = $(id).offset().top;
+
+
+        if(!position){
+            return;
+        }
+
+        return $('html,body').animate({'scrollTop': position }, 'slow', 'swing').promise();
+
+    };
+    /***--------- SCROLL TO Helper ------------ ***/
+
+    app.ScrollToNavigation = function(){
+        this.setEvents();
+    };
+
+    app.ScrollToNavigation.prototype.setEvents = function(){
+        $('.' + app.ScrollToNavigation.SCROLL_TO_NAV_CLASS).on('click', this.scrollToElement.bind(this));
+    };
+
+    app.ScrollToNavigation.prototype.scrollToElement = function(event){
+        event.preventDefault();
+
+        var id = $(event.target).attr('href');
+        app.ScrollToHelper.scrollTo(id);
+
+        app.ScrollToHelper.scrollTo(id);
+    };
+
+    app.ScrollToNavigation.SCROLL_TO_NAV_CLASS = 'nav-scrollto';
+
+    $(document).ready(function(){
+        var scrollToNav = new app.ScrollToNavigation();
+    });
+})(jQuery);
