abb98af38ac198430307108ae279dfdc62e09c69
Michael Tran <koramaiku@Michaels-iMac.local>
2015-08-13 00:07:51 +1000

    Background Images Complete

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('styles', function(){
 
 // Image optimization task
 gulp.task('images', function(){
-	gulp.src('site/img/*')
+	gulp.src('site/img/**/*')
 	.pipe(imagemin())
 	.pipe(gulp.dest('build/img'));
 });
